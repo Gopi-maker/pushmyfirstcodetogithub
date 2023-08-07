13b21aa92a7d13b7ec32781dc39f4cba529a8df5
Gopi-maker <gopibollepalli6@gmail.com>
2023-08-07 23:32:53 +0530

    addhtml

---
+++ dom.js
@@ -0,0 +1,22 @@
+//console.dir(document);
+//onsole.log(document.domain);
+const headerTitle=document.getElementById("header-title");
+//console.log(headerTitle);
+//headerTitle.textContent="Hello";
+headerTitle.style.borderBottom="solid 3px black";
+//console.log(headerTitle.textContent);
+//const qrty=document.querySelector(".title");
+//console.log(qrty);
+//const items=document.getElementsByClassName("list-group-item");
+//console.log(items);
+//items[1].textContent="gopi";
+const items=document.getElementsByClassName("title");
+//console.log(items);
+//for(let i=0;i<items.length;i++){
+items[0].style.fontWeight="bold";
+items[0].style.color="green";
+//}
+//const header=document.querySelector(".title:nth-child(1)");
+//console.log(header);
+//const h1=document.getElementsByTagName("title");
+//console.log(h1.innerText);
